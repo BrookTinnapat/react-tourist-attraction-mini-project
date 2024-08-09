7ccad3f29cf3b68eb07c0aee6b93f5494c639e77
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-08-09 16:40:10 +0700

    prepare for deplot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
-import PlaceHighlight from "./components/PlaceHighLight";
-import Footer from "./components/Footer";
+import PlaceHighlight from "./components/PlaceHighlight";
 
 function App() {
   return (
     <>
       <Header />
       <PlaceHighlight />
-      {/* <Footer /> */}
-      {/* ADD FOR PULL REQUEST*/}
     </>
   );
 }
